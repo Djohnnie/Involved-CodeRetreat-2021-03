e1bde0d8c313c42be9270682be4cb80933c98225
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2021-03-26 14:09:33 +0100

    SignalR

--- csharp/InvolvedExchangeWorkers/InvolvedExchangeWorkers/Worker.cs
+++ csharp/InvolvedExchangeWorkers/InvolvedExchangeWorkers/Worker.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -10,6 +11,8 @@ namespace InvolvedExchangeWorkers
 {
     public class Worker : BackgroundService
     {
+        private readonly HubConnection _hubConnection;
+
         private readonly ILogger<Worker> _logger;
         private readonly IApiClient _apiClient;
 
@@ -19,12 +22,28 @@ namespace InvolvedExchangeWorkers
         {
             _apiClient = apiClient;
             _logger = logger;
+
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl("https://involvedexchangewebapi20210324153741.azurewebsites.net/InvolvedExchange")
+                .Build();
+
+            _hubConnection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await _hubConnection.StartAsync();
+            };
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+                await _hubConnection.StartAsync(stoppingToken);
+                _hubConnection.On<Guid, String, Double>("CurrencyUpdate", (id, name, value) =>
+                {
+                    _logger.LogInformation($"CurrencyUpdate: {name} [{value:F2}]");
+                });
+
                 var token = await _apiClient.Authenticate();
                 var response = await _apiClient.GetPortfolio(token);
                 var currencies = await _apiClient.GetCurrencies(token);
